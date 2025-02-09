76fee7990fc1ed824545b9a14da323f725893539
Jeremy Kessous <jeremy.kessous@accor.com>
2025-02-09 13:06:58 +0100

    01 Refacto and main entry point defined + compliance with flake8 & pylint

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
-# Purpose: Say hello
+"""
+Author:  Ken Youens-Clark <kyclark@gmail.com>
+Purpose: Say hello
+"""
 
 import argparse
-from subprocess import getstatusoutput, getoutput
 
 
-parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('-n', '--name', metavar='name',
-                    default='World', help='Name to greet')
-args = parser.parse_args()
-print('Hello, ' + args.name + '!')
+def get_args():
+    """Get the command-line arguments"""
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    return parser.parse_args()
+
+
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+if __name__ == '__main__':
+    main()
