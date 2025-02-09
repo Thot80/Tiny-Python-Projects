896b7c2d7d91a9a52cbb2571537b55cca7ac76b1
Jeremy Kessous <jeremy.kessous@accor.com>
2025-02-09 17:55:25 +0100

    02 All tests green, pylint 10, flake8 None

--- 02_crowsnest/crowsnest.py
+++ 02_crowsnest/crowsnest.py
@@ -2,7 +2,7 @@
 """
 Author : Thoth80 <jeremykessous@yahoo.fr>
 Date   : 2025-02-09
-Purpose: Rock the Casbah
+Purpose: Crow's Nest -- choose the correct article
 """
 
 import argparse
@@ -13,38 +13,12 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Crow\'s Nest -- choose the correct article',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+    parser.add_argument('word',
+                        metavar='word',
+                        help='A Word')
 
     return parser.parse_args()
 
@@ -54,17 +28,10 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
-
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    word = args.word
+    char = word[0].lower()
+    article = 'an' if char in 'aeiou' else 'a'
+    print(f'Ahoy, Captain, {article} {word} off the larboard bow!')
 
 
 # --------------------------------------------------
