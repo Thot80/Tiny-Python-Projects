dd6cd587b51bdbe43d75b944629c109d33d52099
Jeremy Kessous <jeremy.kessous@accor.com>
2025-02-12 22:09:31 +0100

    05 CLI Args defined

--- 05_howler/howler.py
+++ 05_howler/howler.py
@@ -2,7 +2,7 @@
 """
 Author : Thoth80 <jeremykessous@yahoo.fr>
 Date   : 2025-02-12
-Purpose: Rock the Casbah
+Purpose: Howler (upper-cases input)
 """
 
 import argparse
@@ -13,38 +13,21 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Howler (upper-cases input)',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
+    parser.add_argument('text',
+                        metavar='text',
+                        help='Input string or file')
 
     parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        '--output',
+                        metavar='str',
+                        help='Output filename',
+                        default='',
+                        )
+
+    
 
     return parser.parse_args()
 
@@ -54,17 +37,6 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
-
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
 
 
 # --------------------------------------------------
