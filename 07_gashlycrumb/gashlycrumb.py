a1ddc24979e8dec2397a2cd78162fa33670fa610
Jeremy Kessous <jeremy.kessous@accor.com>
2025-03-07 16:22:53 +0100

    07 All Greens

--- 07_gashlycrumb/gashlycrumb.py
+++ 07_gashlycrumb/gashlycrumb.py
@@ -2,7 +2,7 @@
 """
 Author : Thoth80 <jeremykessous@yahoo.fr>
 Date   : 2025-03-07
-Purpose: Rock the Casbah
+Purpose: Gashlycrumb
 """
 
 import argparse
@@ -13,38 +13,21 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Gashlycrumb',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
+    parser.add_argument('letter',
+                        metavar='letter',
+                        help='Letter(s)',
+                        nargs='+')
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
+    
     parser.add_argument('-f',
                         '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        metavar='file',
+                        help='Input file',
+                        type=argparse.FileType(mode='rt', encoding='UTF-8'),
+                        default='gashlycrumb.txt')
 
     return parser.parse_args()
 
@@ -54,19 +37,15 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    letter_to_line = {}
+    
+    for line in args.file:
+        letter_to_line[line[0].lower()] = line.strip()
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
-
-
+    for letter in args.letter:
+        print(letter_to_line.get(letter.lower(), f'I do not know "{letter}".'))
+    
+    
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
